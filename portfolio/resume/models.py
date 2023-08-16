a79e8e482f5be523d3475a274c003a80defbe33f
asryanani1997 <asryan.ani1997@gmail.com>
2023-08-16 06:42:31 +0400

    Minor Changes

--- portfolio/resume/models.py
+++ portfolio/resume/models.py
@@ -45,6 +45,9 @@ class Message(models.Model):
     subject=models.CharField(max_length=100)
     message=models.TextField(max_length=1000)
 
+    def __str__(self) -> str:
+        return f"{self.email}"
+
 class PortfolioProject(models.Model):
     name = models.CharField(max_length=50)
     image = models.ImageField(upload_to="media")
